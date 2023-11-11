5d2468a0ab997984ce2eb5425f07891aca2d9cbd
waiyan150 <46592143+waiyan150@users.noreply.github.com>
2023-11-11 16:45:27 +0630

    feat(app): Initial commit

---
+++ src/main/java/com/salarysurvey/repository/SalarySurveyRepository.java
@@ -0,0 +1,9 @@
+package com.salarysurvey.repository;
+
+import com.salarysurvey.model.SalarySurvey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SalarySurveyRepository extends JpaRepository<SalarySurvey, Integer> {
+}
