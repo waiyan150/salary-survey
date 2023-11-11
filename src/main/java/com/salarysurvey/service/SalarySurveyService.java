aec664e0062a1261c1f583acdcf6c114a7effe33
waiyan150 <46592143+waiyan150@users.noreply.github.com>
2023-11-11 18:15:54 +0630

    feat(app): before adding filter

--- src/main/java/com/salarysurvey/service/SalarySurveyService.java
+++ src/main/java/com/salarysurvey/service/SalarySurveyService.java
@@ -1,11 +1,30 @@
 package com.salarysurvey.service;
 
-
 import com.salarysurvey.model.SalarySurvey;
+import com.salarysurvey.model.SalarySurveyList;
+import com.salarysurvey.repository.SalarySurveyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface SalarySurveyService {
-    public SalarySurvey save(SalarySurvey entity);
-    public List<SalarySurvey> getAll();
+@Service
+public class SalarySurveyService {
+
+    @Autowired
+    private SalarySurveyRepository repository;
+
+    public SalarySurvey getOne(final Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public SalarySurveyList getAll(Pageable pageable) {
+        Page<SalarySurvey> results = repository.findAll(pageable);
+        return SalarySurveyList.builder()
+                .numberOfItems(results.getTotalElements()).numberOfPages(results.getTotalPages())
+                .salarySurveys(results.getContent())
+                .build();
+    }
 }
