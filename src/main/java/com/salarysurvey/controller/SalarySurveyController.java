6520361c003e113195b20608f54c9d3e51692f9e
waiyan150 <46592143+waiyan150@users.noreply.github.com>
2023-11-11 23:40:30 +0630

    feat(app): Add search

--- src/main/java/com/salarysurvey/controller/SalarySurveyController.java
+++ src/main/java/com/salarysurvey/controller/SalarySurveyController.java
@@ -2,13 +2,17 @@ package com.salarysurvey.controller;
 
 import com.salarysurvey.model.SalarySurvey;
 import com.salarysurvey.model.SalarySurveyModelAssembler;
+import com.salarysurvey.model.SalarySurveySpecificationsBuilder;
 import com.salarysurvey.service.SalarySurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/survey")
@@ -27,8 +31,15 @@ public class SalarySurveyController {
     }
 
     @GetMapping()
-    public PagedModel<SalarySurvey> getAll(Pageable pageable) {
-        Page<SalarySurvey> results = service.getAll(pageable);
-        return pagedResourcesAssembler.toModel(results, assembler);
+    public PagedModel<SalarySurvey> search(@RequestParam(required = false, value = "search") String search, Pageable pageable) {
+        SalarySurveySpecificationsBuilder builder = new SalarySurveySpecificationsBuilder();
+        Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
+        Matcher matcher = pattern.matcher(search + ",");
+        while (matcher.find()) {
+            builder.with(matcher.group(1), matcher.group(2), matcher.group(3));
+        }
+
+        Specification<SalarySurvey> spec = builder.build();
+        return pagedResourcesAssembler.toModel(service.findAllWithFilter(spec, pageable), assembler);
     }
 }
---
+++ src/main/java/com/salarysurvey/model/SalarySurveySpecification.java
@@ -0,0 +1,44 @@
+package com.salarysurvey.model;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
+
+public class SalarySurveySpecification implements Specification<SalarySurvey> {
+
+    private SpecSearchCriteria criteria;
+
+    public SalarySurveySpecification(final SpecSearchCriteria criteria) {
+        super();
+        this.criteria = criteria;
+    }
+
+    public SpecSearchCriteria getCriteria() {
+        return criteria;
+    }
+    @Override
+    public Predicate toPredicate(final Root<SalarySurvey> root, final CriteriaQuery<?> query, final CriteriaBuilder builder) {
+        switch (criteria.getOperation()) {
+            case EQUALITY:
+                return builder.equal(root.get(criteria.getKey()), criteria.getValue());
+            case NEGATION:
+                return builder.notEqual(root.get(criteria.getKey()), criteria.getValue());
+            case GREATER_THAN:
+                return builder.greaterThan(root.get(criteria.getKey()), criteria.getValue().toString());
+            case LESS_THAN:
+                return builder.lessThan(root.get(criteria.getKey()), criteria.getValue().toString());
+            case LIKE:
+                return builder.like(root.get(criteria.getKey()), criteria.getValue().toString());
+            case STARTS_WITH:
+                return builder.like(root.get(criteria.getKey()), criteria.getValue() + "%");
+            case ENDS_WITH:
+                return builder.like(root.get(criteria.getKey()), "%" + criteria.getValue());
+            case CONTAINS:
+                return builder.like(root.get(criteria.getKey()), "%" + criteria.getValue() + "%");
+            default:
+                return null;
+        }
+    }
+}
